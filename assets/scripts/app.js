cc6185a8372c8870b5a741fc0a9280c3574b5708
Brian Artea <brian@expertdesign.cc>
2020-08-04 22:43:07 -0400

    8-Defining-a-function-and-call-it

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,18 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert('The result is ' + result);
+}
+
+add(1, 5);
+add(6, 10);
+add(6, 20);
+add(6, 30);
+
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
-outputResult(currentResult, errorMessage);
+outputResult(currentResult, calculationDescription);
